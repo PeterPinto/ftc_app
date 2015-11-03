fed46f8ff3c924fbebd7edd957f048408ce5ca3d
Peter Pinto <peter.pinto.student@oratoryprep.org>
2015-11-03 10:59:47 -0500

    Added comments for clarity

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Robot.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Robot.java
@@ -5,9 +5,19 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  */
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.*;
 import com.qualcomm.robotcore.util.Range;
+
+/*
+ * This class was created so that code can be easily reused for new robots.  This is not an OpMode, but may
+ * be implemented in one by creating an object of this class.
+ *
+ * The class contains support for Tank and Arcade drive modes, and is ready for Omni-wheel drive to be implemented.
+ *
+ * Currently, there is only support for one motor on the left and one on the right.  If more than one motor is desired, attach
+ * both motors on the left side into the same channel, and attach both motors on the right side into a channel together.
+ */
+
 
 public class Robot {
 
@@ -32,11 +42,13 @@ public class Robot {
         initRobot();
     }
 
+    //Call any important functions at initialization time.  Also set any fields that are unset by the constructor
     private void initRobot()
     {
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
     }
 
+    //Allow for the the robot to be passed the gamepad object instead of setting values directly.
     public void drive(Gamepad gamepad)
     {
         double leftX  = gamepad.left_stick_x;
@@ -47,10 +59,12 @@ public class Robot {
         drive(leftX, leftY, rightX, rightY);
     }
 
+    //Takes drive values and converts them into motor power values
     public void drive(double leftX, double leftY, double rightX, double rightY)
     {
         double leftPower, rightPower;
 
+        //Execute different drive code based on the driveType field
         switch(driveType)
         {
             case TANK_DRIVE:
@@ -79,6 +93,8 @@ public class Robot {
                 rightPower = leftY - leftX;
                 break;
 
+            //Omni drive not yet implemented
+
             default:
                 leftPower  = 0;
                 rightPower = 0;
@@ -88,6 +104,7 @@ public class Robot {
         setMotorPower(leftPower, rightPower);
     }
 
+    //Sets the power of the motors
     private void setMotorPower(double left, double right)
     {
         //Clip values to prevent errors
@@ -97,6 +114,8 @@ public class Robot {
         leftMotor.setPower(left);
         rightMotor.setPower(right);
     }
+
+    //Public get / set methods ***************************
 
     public boolean inputsSquared()
     {
@@ -117,4 +136,6 @@ public class Robot {
     {
         squareInputs = square;
     }
+
+    //****************************************************
 }
